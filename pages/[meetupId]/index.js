fa5d3b61198f617e00781ee239746362b4c25150
riyoneri <lionkaneza@gmail.com>
2023-02-09 18:40:16 +0200

    get static paths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,12 +1,53 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail"
 
-const MeetupDetails = () => {
+const MeetupDetails = props => {
     return <MeetupDetail
-        image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-        title="First meetup"
-        address="Some Street 5, Some city"
-        description="This is a first meetup"
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
     />
+}
+
+export const getStaticPaths = () => {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: "m1",
+                }
+            },
+            {
+                params: {
+                    meetupId: "m2",
+                }
+            },
+            {
+                params: {
+                    meetupId: "m3",
+                }
+            }
+        ]
+    }
+}
+
+export const getStaticProps = context => {
+
+    const meetupId = context.params.meetupId
+
+    console.log(meetupId)
+
+    return {
+        props: {
+            meetupData: {
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+                title: "First meetup",
+                address: "Some Street 5, Some city",
+                description: "This is a first meetup",
+            }
+        }
+    }
 }
 
 export default MeetupDetails

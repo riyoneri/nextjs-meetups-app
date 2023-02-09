b71d5d5498fd1d5b4c35614da6c3ec254ac2866b
riyoneri <lionkaneza@gmail.com>
2023-02-09 16:23:05 +0200

    Add pre render datas on page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail"
+
+const MeetupDetails = () => {
+    return <MeetupDetail
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+        title="First meetup"
+        address="Some Street 5, Some city"
+        description="This is a first meetup"
+    />
+}
+
+export default MeetupDetails
